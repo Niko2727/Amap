68dfc395ea0c946025cac8f5ff797a79d9b90af1
Niko27 <guojunfeng0401@outlook.com>
2024-06-03 19:31:51 +0800

    feat: add Amap sdk

--- app/src/main/java/com/niko/amap/MainActivity.kt
+++ app/src/main/java/com/niko/amap/MainActivity.kt
@@ -2,46 +2,57 @@ package com.niko.amap
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.niko.amap.ui.theme.AmapTheme
+import com.amap.api.maps.AMap
+import com.amap.api.maps.MapView
+import com.amap.api.maps.MapsInitializer
 
 class MainActivity : ComponentActivity() {
+    lateinit var mapView: MapView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            AmapTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        MapsInitializer.updatePrivacyShow(this, true, true)
+        MapsInitializer.updatePrivacyAgree(this, true)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AmapTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
+
+        //获取地图控件引用
+        mapView = findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mapView.onCreate(savedInstanceState);
+
+
+        //初始化地图控制器对象
+        val map: AMap = mapView.getMap()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mapView.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mapView.onPause()
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mapView.onSaveInstanceState(outState)
+    }
+
 }
