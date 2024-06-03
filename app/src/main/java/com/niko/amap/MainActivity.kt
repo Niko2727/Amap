1a87948513e27adc91f0dd6704e3b74ff73142a7
Niko27 <guojunfeng0401@outlook.com>
2024-06-03 19:35:52 +0800

    feat: request position permission

--- app/src/main/java/com/niko/amap/MainActivity.kt
+++ app/src/main/java/com/niko/amap/MainActivity.kt
@@ -2,32 +2,41 @@ package com.niko.amap
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import com.amap.api.maps.AMap
 import com.amap.api.maps.MapView
 import com.amap.api.maps.MapsInitializer
+import com.amap.api.maps.model.MyLocationStyle
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
-    lateinit var mapView: MapView
 
+    // region members
+
+    private lateinit var mapView: MapView
+
+    private lateinit var map: AMap
+
+    private lateinit var locationPermissionRequest: ActivityResultLauncher<Array<String>>
+
+    private lateinit var backgroundLocationPermissionRequest: ActivityResultLauncher<Array<String>>
+
+    // endregion
+
+    // region lifecycle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
-        MapsInitializer.updatePrivacyShow(this, true, true)
-        MapsInitializer.updatePrivacyAgree(this, true)
 
         setContentView(R.layout.activity_main)
 
-        //获取地图控件引用
-        mapView = findViewById(R.id.map);
-        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
-        mapView.onCreate(savedInstanceState);
-
-
-        //初始化地图控制器对象
-        val map: AMap = mapView.getMap()
+        registerPermissionResultReceiver()
+        initPrivacy()
+        initMap(savedInstanceState)
+        checkLocationPermission()
     }
 
     override fun onDestroy() {
@@ -54,5 +63,110 @@ class MainActivity : ComponentActivity() {
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
         mapView.onSaveInstanceState(outState)
     }
+
+    // endregion lifecycle
+
+    // region permission
+
+    private fun registerPermissionResultReceiver() {
+        locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                    requestBackgroundLocationPermission()
+                }
+
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+
+                }
+
+                else -> {
+                    // No location access granted.
+                }
+            }
+        }
+
+        backgroundLocationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_BACKGROUND_LOCATION, false) -> {
+                    // Precise location access granted.
+                }
+
+                else -> {
+                    // No location access granted.
+                }
+            }
+        }
+    }
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+
+    private fun requestBackgroundLocationPermission() {
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        )
+    }
+
+    // endregion
+
+    // region Amap
+
+    private fun initPrivacy() {
+        MapsInitializer.updatePrivacyShow(this, true, true)
+        MapsInitializer.updatePrivacyAgree(this, true)
+    }
+
+    private fun initMap(savedInstanceState: Bundle?) {
+        //获取地图控件引用
+        mapView = findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mapView.onCreate(savedInstanceState);
+
+        //初始化地图控制器对象
+        val map: AMap = mapView.getMap()
+        val locationStyle = MyLocationStyle() //初始化定位蓝点样式类
+
+
+        with(locationStyle) {
+            myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER); //连续定位、蓝点不会移动到地图中心点，定位点依照设备方向旋转，并且蓝点会跟随设备移动。
+            //myLocationStyle.interval(2000) //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+
+        }
+
+
+        with(map) {
+            myLocationStyle = locationStyle //设置定位蓝点的Style
+            isMyLocationEnabled = true // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        }
+
+
+        with(map.uiSettings) {
+            isMyLocationButtonEnabled = true // 设置默认定位按钮是否显示，非必需设置。
+            isZoomControlsEnabled = false    // 设置缩放按钮
+        }
+    }
+
+
+    // endregion
 
 }
